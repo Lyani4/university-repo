860b297d8545fef3f27ae27e11531d3207f567a6
Lyani4 <lyan.sashuta@mail.ru>
2024-09-20 16:14:00 +0000

    solve lesson3

--- lesson3/main.cpp
+++ lesson3/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-  std::cout << "Hello, World!\n";
+  float x;
+  float y;
+
+  std::cout << "Введите число x" << std::endl;
+  std::cin >> x;
+  std::cout << "Введите число y\n";
+  std::cin >> y;
+
+  if (x > 0 && y > 0){
+    std::cout << "Заданная точка в первой четверти\n";
+  } else if ( x < 0 && y > 0) {
+    std::cout << "Заданная точка во второй четверти\n";
+  } else if ( x < 0 && y < 0) {
+    std::cout << "Заданная точка в третьей четверти\n";
+  } else if ( x > 0 && y < 0) {
+    std::cout << "Заданная точка в четвертой четверти\n";
+  }
   return 0;
 }
