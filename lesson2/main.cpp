41544eb8d329c56f52cc7e6894c36c5a6c3abe78
Sashok <lyan.sashuta@mail.ru>
2024-09-27 20:53:48 +0700

    solve lab2

--- lesson2/main.cpp
+++ lesson2/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <cmath>
 
+/*
+Нужно вычислить уравнение (e^sin(x) + ln(arctg (x))) / sin(x)
+ */
 int main() {
+  float x;
+  double expression1;
+  double expression2;
+  double expression3;
 
+  std::cout << "Введите число x\n";
+  std::cin >> x;
+  expression1 = exp(sin(x));
+  expression2 = log(atan(x));
+  expression3 = sin(x);
+  std::cout << (expression1 + expression2)/expression3 << std::endl;
   return 0;
 }
