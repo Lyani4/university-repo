f7d483bd78bb659c3636d57a9b8ce19b54a1e739
Sashok <lyan.sashuta@mail.ru>
2024-09-27 21:44:45 +0700

    solve lab4 №1

--- lesson4/main.cpp
+++ lesson4/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+/*
+Вывести на экран монитора название времени года по его номеру
+ */
 int main() {
-
+  unsigned int season_number;
+  std::cout << "Введите число\n";
+  std::cin >> season_number;
+  switch(season_number % 4) {
+    case 1:
+      std::cout << "Зима\n";
+      break;
+    case 2:
+      std::cout << "Весна\n";
+      break;
+    case 3:
+      std::cout << "Лето\n";
+      break;
+    case 4:
+      std::cout << "Осень\n";
+      break;
+  }
   return 0;
 }
