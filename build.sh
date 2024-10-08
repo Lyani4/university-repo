d5892953c6251d04a55efcf7e18cbde1661895f6
Mihailov Maxim <mihailov.box@mail.ru>
2024-10-08 14:14:58 +0000

    update build.sh script with file check and formating

--- build.sh
+++ build.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/bash
 
 path=$1
-echo "start build"
-clang++ -o "main.o" "./${path}/main.cpp"
-echo "finish build"
+
+exists_file() {
+  if [ -f "${path}" ]; then
+    echo "Файл ${path} существует"
+  else
+    echo "Файл ${path} не существует"
+    exit 1
+  fi
+}
+
+formating_code() {
+  echo "Start formating file ${path}"
+  clang-format -style=file -i "${path}" || { echo "format error"; exit 1; }
+  echo "Success formating file ${path}"
+}
+
+building_source() {
+  echo "Start build file ${path}"
+  clang++ -o "main.o" "${path}" || { echo "build error"; exit 1; }
+  echo "Success build file ${path}"
+}
+
+exists_file
+formating_code
+building_source
