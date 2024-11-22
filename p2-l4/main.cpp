14156662d0f6d461c98e8f8da96ec1e062a7969c
sashok <lyan.sashuta@mail.ru>
2024-11-22 22:20:05 +0700

    update solution

--- p2-l4/main.cpp
+++ p2-l4/main.cpp
@@ -5,7 +5,6 @@
  * произведение ненулевых элементов матрицы в указанном столбце.
  */
 
-
 int main() {
   int stolbec, stroka;
   std::cout << "Введите количество столбцов и строк" << std::endl;
@@ -35,14 +34,22 @@ int main() {
     std::cout << "Такого столбца нету, выход из программы..." << std::endl;
     exit(1);
   }
-  for (int i = 0; i < stolbec; i++) {
-    for (int j = 0; j < stroka; j++) {
-      if (j == column and mas[i][j] != 0) {
-        allZero = false;
-        product *= mas[i][j];
+    for (int i = 0; i < stolbec; i++) {
+      for (int j = 0; j < stroka; j++) {
+        if (j == column and mas[i][j] != 0) {
+          allZero = false;
+          product *= mas[i][j];
+        }
       }
     }
-  }
+
+//  Оптимизированный способ
+//  for (int i = 0; i < stolbec; i++) {
+//    if (mas[i][column] != 0) {
+//      allZero = false;
+//      product *= mas[i][column];
+//    }
+//  }
   if (allZero == true) {
     std::cout << "Произведение столбца " << column << std::endl;
     std::cout << 0 << std::endl;
