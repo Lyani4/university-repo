5d1f3f6be7a26d934ffc0ad1b5cdde16b61f433b
sashok <lyan.sashuta@mail.ru>
2024-11-22 22:10:13 +0700

    solve p2-l4

---
+++ p2-l4/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+/**
+ * Дана матрица. Вывести ее в матричной форме. Вычислить
+ * произведение ненулевых элементов матрицы в указанном столбце.
+ */
+
+
+int main() {
+  int stolbec, stroka;
+  std::cout << "Введите количество столбцов и строк" << std::endl;
+  std::cin >> stolbec >> stroka;
+  int mas[stolbec][stroka];
+  std::cout << "Вводите элементы массива" << std::endl;
+  for (int i = 0; i < stolbec; i++) {
+    for (int j = 0; j < stroka; j++) {
+      std::cout << "Элемент " << i << "," << j << " " << std::endl;
+      std::cin >> mas[i][j];
+    }
+  }
+  std::cout << "Вывод матричной формы:" << std::endl;
+  for (int i = 0; i < stolbec; i++) {
+    for (int j = 0; j < stroka; j++) {
+      std::cout << mas[i][j];
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "Укажите столбец для вычисления произведения(Начиная с 0) "
+            << std::endl;
+  int column;
+  int product = 1;
+  bool allZero = true;
+  std::cin >> column;
+  if (column > stolbec) {
+    std::cout << "Такого столбца нету, выход из программы..." << std::endl;
+    exit(1);
+  }
+  for (int i = 0; i < stolbec; i++) {
+    for (int j = 0; j < stroka; j++) {
+      if (j == column and mas[i][j] != 0) {
+        allZero = false;
+        product *= mas[i][j];
+      }
+    }
+  }
+  if (allZero == true) {
+    std::cout << "Произведение столбца " << column << std::endl;
+    std::cout << 0 << std::endl;
+    exit(0);
+  }
+  std::cout << "Произведение столбца " << column << std::endl;
+  std::cout << product << std::endl;
+}
